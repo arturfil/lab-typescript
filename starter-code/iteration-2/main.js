a67893a96dcbce7215b3090458e48bc8ee1830e8
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-09-25 14:43:11 -0400

    Dharma and Arturo

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,43 @@
-"use strict";
+var Todo = (function () {
+    function Todo(Tasks) {
+        if (Tasks === void 0) { Tasks = []; }
+        this.Tasks = Tasks;
+    }
+    Todo.prototype.addTask = function (string) {
+        this.Tasks.push(string);
+        console.log('=========== NEW TASK ===========');
+        console.log("added '" + string + "' to the list of Tasks");
+        console.log("Number of Items: " + this.Tasks.length);
+        console.log("List of Tasks: ");
+        return this.listAllTasks();
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.Tasks.forEach(function (oneTask) {
+            console.log(oneTask);
+        });
+    };
+    Todo.prototype.deleteTask = function (task) {
+        console.log("=========== TASK REMOVED ===========");
+        console.log("Tasks: " + task + " deleted");
+        task.slice();
+        console.log("Number of items: " + this.Tasks.length);
+    };
+    return Todo;
+}());
+var myTodo = new Todo();
+console.log("Number of items:", myTodo.addTask('This is our first task'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 3!! 🤓'));
+myTodo.listAllTasks();
+console.log("Number of items:", myTodo.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodo.deleteTask('Finish this iteration 2!! 🤓'));
+myTodo.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,15 +1,53 @@
 // Add the reference to the "TodoInterface"
 
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements Todointerface {
+
+  constructor(
+    public Tasks: Array <string> = [],
+  ) {}
+
+  addTask (string: string) {
+    this.Tasks.push(string);
+    console.log('=========== NEW TASK ===========');
+    console.log("added '" + string + "' to the list of Tasks");
+    console.log("Number of Items: " + this.Tasks.length);
+    console.log("List of Tasks: ")
+    return this.listAllTasks();
+  }
+
+  listAllTasks() {
+    this.Tasks.forEach((oneTask) => {
+      console.log(oneTask);
+    })
+  }
+
+  deleteTask(task: string) {
+    console.log("=========== TASK REMOVED ===========");
+    console.log("Tasks: " + task + " deleted");
+    task.slice();
+    console.log("Number of items: " + this.Tasks.length);
+  }
+}
+
+
+
 
 // Execution
-let myTodos = new Todo();
-console.log("Number of items:", myTodos.addTask('This is our first task'));
-console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
-myTodos.listAllTasks();
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
-myTodos.listAllTasks();
+var myTodo = new Todo();
+console.log("Number of items:", myTodo.addTask('This is our first task'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodo.addTask('Finish this iteration 3!! 🤓'));
+myTodo.listAllTasks();
+console.log("Number of items:", myTodo.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodo.deleteTask('Finish this iteration 2!! 🤓'));
+myTodo.listAllTasks();
